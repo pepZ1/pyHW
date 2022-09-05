fa93c611e39d2b3a599e3cd3014cab529eaa5154
aketo <aketo13@gmail.com>
2022-09-05 15:06:06 +0400

    add task5

--- homework1.py
+++ homework1.py
@@ -85,3 +85,38 @@ if quart_number in [1, 2, 3, 4]:
          print("Значения координат точки: х ∈ [1, ∞), y ∈ [-1, -∞)")
 else:
      print("!Введите число от 1 до 4!") """
+
+
+""" Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+Пример:
+
+- A (3,6); B (2,1) -> 5,09
+- A (7,-5); B (1,-1) -> 7,21 """
+
+def inputNumbers(x):
+    xy = ["X", "Y"]
+    a = []
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = int(input(f"Введите координату по {xy[i]}: "))
+                a.append(number)
+                is_OK = True
+            except ValueError:
+                print("Только целые числа")
+    return a
+
+
+def calculateLengthSegment(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Введите координаты точки А")
+pointA = inputNumbers(2)
+print("Введите координаты точки В")
+pointB = inputNumbers(2)
+
+print(f"Расстояние: {format(calculateLengthSegment(pointA, pointB), '.2f')}")
