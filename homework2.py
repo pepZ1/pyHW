ebd0d97ac6599acef91c634e2d1db9e42542da3e
aketo <aketo13@gmail.com>
2022-09-09 14:21:41 +0400

    add task4

--- homework2.py
+++ homework2.py
@@ -19,5 +19,33 @@ for i in range(1, int(input()) + 1):
     summa += ((1 + 1 / i) ** i)
 print(round(summa))
  """
+""" Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+Пример:
+- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4) """
 
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число ")
+    return number
+
+
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+
+num = InputNumbers("Введите число: ")
+
+list = []
+for e in range(1, num + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
 
