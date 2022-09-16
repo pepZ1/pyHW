254e2a28f16411b7f555f3b16c2107cd08f809be
aketo <aketo13@gmail.com>
2022-09-16 17:21:20 +0400

    add task2

--- homework4.py
+++ homework4.py
@@ -4,10 +4,27 @@
 
 - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$ """
 
-acc = list(input("Задайте точность для вычисления pi: "))
+""" acc = list(input("Задайте точность для вычисления pi: "))
 
 n = len(acc)-2
 
 pi = round(sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(n)), n)
 
-print(pi)
+print(pi) """
+
+""" Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N. """
+
+num = int(input('Введите число: '))
+def fac(n):
+    delitor = []
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            n //= i 
+            delitor.append(i)
+        i += 1
+    if n > 1:
+        delitor.append(n)
+    return delitor
+
+print(fac(num))
