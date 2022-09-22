0548feef320ef1b98cf82349d77b47c7a1dd8c5c
aketo <aketo13@gmail.com>
2022-09-22 17:21:32 +0400

    add task1

---
+++ homework5.py
@@ -0,0 +1,11 @@
+""" Напишите программу, удаляющую из текста все слова, содержащие ""абв"". """
+
+""" word = 'Сначала я абв как абвпонял, а потом понял!'
+find = 'абв'
+
+
+def del_words(letters, del_text):
+    letters = list(filter(lambda x: del_text not in x, letters.split()))
+    return " ".join(letters)
+print(del_words(word, find)) """
+
